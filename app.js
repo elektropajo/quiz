5b7f9539d8b49e4a26a769c28ddeb5493ba4d8cb
Andrés Bayón <andres.bayon@riseup.net>
2015-05-28 18:25:36 +0200

    Añadidas hojas de estilo y responsividad usando LESS.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials'); //añadido
 
 /* ENRUTADORES */
 
@@ -20,6 +21,8 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 /* INSTALACIÓN DE MIDDLEWARES */
 
