1128c8fee4f9b38d208e8cab1489b8904c9ac763
Andrés Bayón <andres.bayon@riseup.net>
2015-06-05 22:03:11 +0200

    Búsqueda de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,51 @@
 var models = require('../models/models.js');
+var QS = require('querystring');
+
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then( function (quiz) {
+    if (quiz) {
+      req.quiz = quiz;
+      next();
+    } else {
+      next(new Error('No existe quizId=' + quizId));
+    }
+  });
+}
+
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes });
-    console.log('Quizes:'+ quizes.lenght);
-  });
+  if (req.query.search) {
+    var search = req.query.search;
+    search = '%' + search.replace(" ", "%") + '%';
+    models.Quiz.findAll({
+      where: {
+        pregunta: {
+          $like: '%' + search
+        }
+      },
+      order: ['pregunta']
+    }).then(function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes, search: req.query.search });
+    });
+  } else {
+    models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index.ejs', { quizes: quizes, search: '' });
+    });
+  }
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: quiz });
-  });
+    res.render('quizes/show', { quiz: req.quiz });
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto' });
-    } else {
-      res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto' });
-    }
-  });
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
